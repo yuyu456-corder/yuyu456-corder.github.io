7d8edc7b112fd26ffb7ce8e283ed25ea7a85f79e
yuyu456 <youq27668@gmail.com>
2020-06-17 20:07:59 +0900

    save: english_notesのリポジトリ詳細ページに画像やUML図を追加

--- my_portfolio_site/pages/aboutRepositories/aboutEnglishNotes.vue
+++ my_portfolio_site/pages/aboutRepositories/aboutEnglishNotes.vue
@@ -26,10 +26,16 @@ v-app
 
     p ユーザ毎に学習の進捗度や正答率を表示させたいためサーバとの連携も考えています。
 
+    h2(style="border: 1px solid black; box-shadow: 30px -8px #4169E1 ") ＞システム構成について
+
+    v-contaniner(class="ma-2")
+    img(src="/activityDiagram.png",alt="アクティビティUML図",width="30%")
+    p 左図に示したアクティビティ図のように本システムは動作します。
+
   h2(style="border: 1px solid black; box-shadow: 30px -8px #4169E1 ") ＞開発時に自分が苦労した点
+
   v-contaniner(class="ma-2")
-  h3 文字列リテラルの理解（ポインタの復習）
-  v-contaniner(class="ma-2")
+    h3 文字列リテラルの理解（ポインタの復習）
     p Cで文字列を扱うには配列の利用が不可欠のため、それに伴いポインタの復習になりました。
     | 今回はJSONファイルからKeyとValueを取り出す必要がありましたが、
     <br/>
@@ -38,11 +44,27 @@ v-app
     | アドレス経由で文字を扱うにはポインタが不可欠なのでこの機能を実装する際に同時にポインタの復習になりました。
   
   v-contaniner(class="ma-2")
-  h3 マルチバイト文字の扱いについて
-  p Cでは当然ではメモリの使用量に厳密に取り扱うため日本語（マルチバイト文字）の取り扱いに苦戦しました。
-  | JSONファイルを1行ずつ読み込み、１つの配列に代入して使用したメモリを計算するコードを書いてきた時、
-  <br/>
-  | マルチバイト文字を考慮しておらず計算結果が自分の思っていた結果と一致しておらずデバッグ作業に時間を取られました。
+    h3 マルチバイト文字の扱いについて
+    p Cでは当然ではメモリの使用量に厳密に取り扱うため日本語（マルチバイト文字）の取り扱いに苦戦しました。
+    | JSONファイルを1行ずつ読み込み、１つの配列に代入して使用したメモリを計算するコードを書いてきた時、
+    <br/>
+    | マルチバイト文字を考慮しておらず計算結果が自分の思っていた結果と一致しておらずデバッグ作業に時間を取られました。
+
+  h2(style="border: 1px solid black; box-shadow: 30px -8px #4169E1 ") ＞実際のアプリケーションの画面
+  v-contaniner(class="ma-2")
+  p 開発途中の為、まだCLI上での出力にしか対応できていません・・・。
+
+  v-contaniner(class="ma-2")
+  img(src="/english_notes_screenshot.png",alt="JSONパースのログ",width="50%")
+  p JSONファイルからKey（英単語）とValue（日本語訳）をパースしているログ
+
+  v-contaniner(class="ma-2")
+  img(src="/english_notes_screenshot2.png",alt="問題モードの画面",width="50%")
+  p 問題モード：4択から正解を選択していくモード
+
+  v-contaniner(class="ma-2")
+  img(src="/english_notes_screenshot3.png",alt="英単語帳モードの画面",width="50%")
+  p 英単語帳モード：ひたすら英単語とその日本語訳が表示されるモード
 
 </template>
 
